880636b79299157400bdd2a4d0ef6ee351a6f38c
Roman Milata <roman.milata.za@gmail.comgit config --global user.email roman.milata.za@gmail.comgit config --global user.email roman.milata.za@gmail.com>
2024-08-06 09:38:05 +0200

    after 122. Implementing Lifecycle Interfaces

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
   // currentStatus = 'online';
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
 
   constructor() {
+
+  }
+
+  ngOnInit() {
     setInterval (() => {
       const rnd = Math.random();
 
